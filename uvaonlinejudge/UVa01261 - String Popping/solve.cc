d451d377a02ba1c27d57bd559b0123823679e302
Adrian Serrano <adri.sr.83@gmail.com>
2015-12-23 01:06:33 +0100

    UVa01261 - String Popping

---
+++ uvaonlinejudge/UVa01261 - String Popping/solve.cc
@@ -0,0 +1,87 @@
+#include "../../helper/helper.1.h"
+
+std::map<Vi,bool> memo;
+
+Vi stringToV(const std::string& s) {
+    int count = 1,
+        len = s.size();
+    Vi result;
+    result.reserve(s.size());
+    char c = s[0];
+    bool sign = false;
+    for (int i=1; i < len; ++i) {
+        if (s[i] == c) {
+            ++count;
+        }
+        else {
+            c = s[i];
+            result.push_back(sign? count : -count);
+            count = 1;
+            sign = !sign;
+        }
+    }
+    if (count)
+        result.push_back(sign? count : -count);
+    return result;
+}
+
+void erase_at(Vi& vect, int pos) {
+    int lastpos = vect.size() - 1;
+    if (pos>0 && pos<lastpos) {
+        vect[pos-1] += vect[pos+1];
+        vect.erase(vect.begin()+pos, vect.begin()+pos+2);
+    }
+    else
+        vect.erase(vect.begin()+pos);
+}
+
+bool backtrack(const Vi& vect) {
+    const auto it = memo.find(vect);
+    if (it != memo.end()) {
+        return it->second;
+    }
+    
+    if (vect.empty())
+        return true;
+
+    bool retval = false;
+    for (int i = 0, len = vect.size(); i < len; ++i) 
+        if (vect[i]>1 || vect[i]<-1) {
+            Vi tmp(vect);
+            erase_at(tmp,i);
+            if (backtrack(tmp)) {
+                retval = true;
+                break;
+            }
+        }
+    memo[vect] = retval;
+    return retval;
+}
+
+bool solve(const std::string& s) {
+    if (s.size()) {
+        Vi problem(stringToV(s));
+        //helper::report("problem",problem);
+        return backtrack(problem);
+    }
+    return true;
+}
+
+int main() {
+    helper::BufferedStdout output;
+    helper::LineReader<> reader;
+    std::string line;
+    
+    reader.get_line(line);
+    const int NUM_CASES = helper::string_to_int(line);
+
+    char result[2] = {'0','\n'};
+    for (int ncase = 1; ncase <= NUM_CASES; ++ncase) {
+        
+        reader.get_line(line);
+        result[0] = solve(line)? '1' : '0';
+        output.append(result,2);
+
+    }
+}
+
